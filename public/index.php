d4e83aa7896ce52d890aea38015a576987e16d35
TIm St.Clair <tim.stclair@gmail.com>
2024-01-30 13:03:46 +1100

    removed debug stuff

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Authorization, Content-Type, Cache-Control, X-Requested-With");
 header("Access-Control-Allow-Methods: GET, PUT, OPTIONS, HEAD");
 
- ini_set ('display_errors', 1); ini_set ('display_startup_errors', 1); error_reporting (E_ALL);
+// ini_set ('display_errors', 1); ini_set ('display_startup_errors', 1); error_reporting (E_ALL);
 
 require_once("../vendor/autoload.php");
 
