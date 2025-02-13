9ab2c313938639e86e6ef62cde1c1e3c4ba60ff8
frumbert <592257+frumbert@users.noreply.github.com>
2025-02-13 11:40:49 +1100

    Updated files to support new Rise changes

--- resources/textentry.js
+++ resources/textentry.js
@@ -6,34 +6,52 @@
  * it is multi-user aware, so it can be used by multiple users on the same course without overwriting data
  */
 
-// This script is generic and exeucutes on timeline start AND on submit button click
+// This script is generic and exeucutes on timeline start (load) AND on submit button click (store)
 // the following variable denotes the action to take
-const Action = 'store'; // load || store
+// valid actions are 'load' and 'store'
+const Action = 'store';
 
 /* ------------- don't edit below here ------------- */
 
-// a reasonably fast md5 implementation for strings
-function md5(n){var r="0123456789abcdef";function t(n){var t,u="";for(t=0;t<=3;t++)u+=r.charAt(n>>8*t+4&15)+r.charAt(n>>8*t&15);return u}function u(n,r){var t=(65535&n)+(65535&r);return(n>>16)+(r>>16)+(t>>16)<<16|65535&t}function e(n,r,t,e,f,o){return u(function(n,r){return n<<r|n>>>32-r}(u(u(r,n),u(e,o)),f),t)}function f(n,r,t,u,f,o,c){return e(r&t|~r&u,n,r,f,o,c)}function o(n,r,t,u,f,o,c){return e(r&u|t&~u,n,r,f,o,c)}function c(n,r,t,u,f,o,c){return e(r^t^u,n,r,f,o,c)}function a(n,r,t,u,f,o,c){return e(t^(r|~u),n,r,f,o,c)}var i,h,v,g,l,A=function(n){var r,t=1+(n.length+8>>6),u=new Array(16*t);for(r=0;r<16*t;r++)u[r]=0;for(r=0;r<n.length;r++)u[r>>2]|=n.charCodeAt(r)<<r%4*8;return u[r>>2]|=128<<r%4*8,u[16*t-2]=8*n.length,u}(""+n),d=1732584193,b=-271733879,m=-1732584194,w=271733878;for(i=0;i<A.length;i+=16)h=d,v=b,g=m,l=w,d=f(d,b,m,w,A[i+0],7,-680876936),w=f(w,d,b,m,A[i+1],12,-389564586),m=f(m,w,d,b,A[i+2],17,606105819),b=f(b,m,w,d,A[i+3],22,-1044525330),d=f(d,b,m,w,A[i+4],7,-176418897),w=f(w,d,b,m,A[i+5],12,1200080426),m=f(m,w,d,b,A[i+6],17,-1473231341),b=f(b,m,w,d,A[i+7],22,-45705983),d=f(d,b,m,w,A[i+8],7,1770035416),w=f(w,d,b,m,A[i+9],12,-1958414417),m=f(m,w,d,b,A[i+10],17,-42063),b=f(b,m,w,d,A[i+11],22,-1990404162),d=f(d,b,m,w,A[i+12],7,1804603682),w=f(w,d,b,m,A[i+13],12,-40341101),m=f(m,w,d,b,A[i+14],17,-1502002290),d=o(d,b=f(b,m,w,d,A[i+15],22,1236535329),m,w,A[i+1],5,-165796510),w=o(w,d,b,m,A[i+6],9,-1069501632),m=o(m,w,d,b,A[i+11],14,643717713),b=o(b,m,w,d,A[i+0],20,-373897302),d=o(d,b,m,w,A[i+5],5,-701558691),w=o(w,d,b,m,A[i+10],9,38016083),m=o(m,w,d,b,A[i+15],14,-660478335),b=o(b,m,w,d,A[i+4],20,-405537848),d=o(d,b,m,w,A[i+9],5,568446438),w=o(w,d,b,m,A[i+14],9,-1019803690),m=o(m,w,d,b,A[i+3],14,-187363961),b=o(b,m,w,d,A[i+8],20,1163531501),d=o(d,b,m,w,A[i+13],5,-1444681467),w=o(w,d,b,m,A[i+2],9,-51403784),m=o(m,w,d,b,A[i+7],14,1735328473),d=c(d,b=o(b,m,w,d,A[i+12],20,-1926607734),m,w,A[i+5],4,-378558),w=c(w,d,b,m,A[i+8],11,-2022574463),m=c(m,w,d,b,A[i+11],16,1839030562),b=c(b,m,w,d,A[i+14],23,-35309556),d=c(d,b,m,w,A[i+1],4,-1530992060),w=c(w,d,b,m,A[i+4],11,1272893353),m=c(m,w,d,b,A[i+7],16,-155497632),b=c(b,m,w,d,A[i+10],23,-1094730640),d=c(d,b,m,w,A[i+13],4,681279174),w=c(w,d,b,m,A[i+0],11,-358537222),m=c(m,w,d,b,A[i+3],16,-722521979),b=c(b,m,w,d,A[i+6],23,76029189),d=c(d,b,m,w,A[i+9],4,-640364487),w=c(w,d,b,m,A[i+12],11,-421815835),m=c(m,w,d,b,A[i+15],16,530742520),d=a(d,b=c(b,m,w,d,A[i+2],23,-995338651),m,w,A[i+0],6,-198630844),w=a(w,d,b,m,A[i+7],10,1126891415),m=a(m,w,d,b,A[i+14],15,-1416354905),b=a(b,m,w,d,A[i+5],21,-57434055),d=a(d,b,m,w,A[i+12],6,1700485571),w=a(w,d,b,m,A[i+3],10,-1894986606),m=a(m,w,d,b,A[i+10],15,-1051523),b=a(b,m,w,d,A[i+1],21,-2054922799),d=a(d,b,m,w,A[i+8],6,1873313359),w=a(w,d,b,m,A[i+15],10,-30611744),m=a(m,w,d,b,A[i+6],15,-1560198380),b=a(b,m,w,d,A[i+13],21,1309151649),d=a(d,b,m,w,A[i+4],6,-145523070),w=a(w,d,b,m,A[i+11],10,-1120210379),m=a(m,w,d,b,A[i+2],15,718787259),b=a(b,m,w,d,A[i+9],21,-343485551),d=u(d,h),b=u(b,v),m=u(m,g),w=u(w,l);return t(d)+t(b)+t(m)+t(w)}
-
-// reference to the storyline player
 const STORYLINE = GetPlayer();
 const VARIABLE = 'TextEntry';
 const BLOCK = window.name;
-async function GetServerLocation() {
-    return new Promise((resolve, reject) => {
-        fetch('/local/blobfiledb/', { method: 'HEAD' })
-        .then((response) => {
-            if (response.ok) {
-                resolve('/local/blobfiledb/');
-            } else {
-                resolve('https://blob.frumbert.org');
-            }
-        }).catch((error) => {
-            resolve('https://blob.frumbert.org');
-        });
+const BACKEND_URL = '/local/blobstorebackend';
+const FALLBACK_URL = 'https://blob.frumbert.org';
+let HAS_BACKEND = false;
+
+// https://stackoverflow.com/a/74186696
+function MD5(r) {
+    var o, e, n, f = [ -680876936, -389564586, 606105819, -1044525330, -176418897, 1200080426, -1473231341, -45705983, 1770035416, -1958414417, -42063, -1990404162, 1804603682, -40341101, -1502002290, 1236535329, -165796510, -1069501632, 643717713, -373897302, -701558691, 38016083, -660478335, -405537848, 568446438, -1019803690, -187363961, 1163531501, -1444681467, -51403784, 1735328473, -1926607734, -378558, -2022574463, 1839030562, -35309556, -1530992060, 1272893353, -155497632, -1094730640, 681279174, -358537222, -722521979, 76029189, -640364487, -421815835, 530742520, -995338651, -198630844, 1126891415, -1416354905, -57434055, 1700485571, -1894986606, -1051523, -2054922799, 1873313359, -30611744, -1560198380, 1309151649, -145523070, -1120210379, 718787259, -343485551 ], t = [ o = 1732584193, e = 4023233417, ~o, ~e ], c = [], a = unescape(encodeURI(r)) + "\u0080", d = a.length;
+    for (r = --d / 4 + 2 | 15, c[--r] = 8 * d; ~d; ) c[d >> 2] |= a.charCodeAt(d) << 8 * d--;
+    for (i = a = 0; i < r; i += 16) {
+        for (d = t; 64 > a; d = [ n = d[3], o + ((n = d[0] + [ o & e | ~o & n, n & o | ~n & e, o ^ e ^ n, e ^ (o | ~n) ][d = a >> 4] + f[a] + ~~c[i | 15 & [ a, 5 * a + 1, 3 * a + 5, 7 * a ][d]]) << (d = [ 7, 12, 17, 22, 5, 9, 14, 20, 4, 11, 16, 23, 6, 10, 15, 21 ][4 * d + a++ % 4]) | n >>> -d), o, e ]) o = 0 | d[1], 
+        e = d[2];
+        for (a = 4; a; ) t[--a] += d[a];
+    }
+    for (r = ""; 32 > a; ) r += (t[a >> 3] >> 4 * (1 ^ a++) & 15).toString(16);
+    return r;
+}
+
+// check to see if we have a local backend or need to use an online fallback
+function GetServerLocation() {
+    if (HAS_BACKEND) return window.location.origin + BACKEND_URL;
+    return fetch(BACKEND_URL + '/', {
+    	method: 'HEAD',
+        headers: { 'Authorization': MD5(location.origin) }
+    })
+    .then(r => {
+        if (r.ok) {
+            HAS_BACKEND = true;
+        	return window.location.origin + BACKEND_URL; // moodle plugin exists
+        }
+        throw new Error;
+    }).catch((error) => {
+        return FALLBACK_URL; // fallback to public server
     });
 }
-let SERVER = await GetServerLocation();
+let SERVER = GetServerLocation();
+
 
 // test for cross domain script access
 function isCrossDomain() {
@@ -56,7 +74,7 @@ function temporaryId(kind) {
 
 function findRuntimeWindow(win) {
     try {
-        if (win.hasOwnProperty("courseData") || win.hasOwnProperty("courseId")) return win;
+        if (win.hasOwnProperty("Rise") || win.hasOwnProperty("courseData") || win.hasOwnProperty("courseId")) return win;
         else if (win.parent == win) return null;
         else return findRuntimeWindow(win.parent);
     } catch (e) {
@@ -75,8 +93,24 @@ function findLMSAPI(win) {
 }
 
 function createDigest(str) {
-    // could also md5 the value to ensure the data is abstracted before being transmitted
-    return btoa(encodeURIComponent(str)).replace(/\//g,'_').replace(/\+/g,'-').replace(/\=/g,'-');
+    return btoa(encodeURIComponent(str)).replace(/\//g,'_').replace(/\+/g,'.').replace(/\=/g,'-');
+    return MD5(str);
+}
+
+function getLocalTime() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    return year + (month < 10 ? '0' : '') + month + (day < 10 ? '0' : '') + day + (hours < 10 ? '0' : '') + hours + (minutes < 10 ? '0' : '') + minutes + (seconds < 10 ? '0' : '') + seconds;
+}
+
+function getInstance() {
+    if (isCrossDomain()) return createDigest(window.location.href);
+    return createDigest(window.parent.location.href);
 }
 
 // set some properties based on content in the PARENT frame, where accessible
@@ -84,22 +118,31 @@ let courseTitle = window.document.title;
 let contextId = temporaryId('context');
 let pageTitle = '-';
 let questionText = '-';
+let titleSource = 'unknown';
+let inst = getInstance();
 const runtimeWindow = findRuntimeWindow(this);
+
+// try turning my background transparent (modern,classic)
+if (!document.body.querySelector('#stylepatch')) {
+    let mystyle = document.createElement('style');
+    mystyle.id = 'stylepatch';
+    mystyle.textContent = `body,#wrapper,.slide .dropin-wrap rect[id^="slide-bg-"],svg[data-display-name='SlideBackground'] stop { background-color:transparent!important;stop-color:transparent!important;fill:none!important;}`;
+    document.body.appendChild(mystyle);
+}
 
 if (window.parent !== window.self && !isCrossDomain()) {
     const IFRAME = parent.document.querySelector(`iframe[name="${BLOCK}"]`);
 
-    // try matching the background colour of the RISE block
     try {
-        if (!document.querySelector('#stylepatch')) {
-            let style = parent.window.getComputedStyle(IFRAME.closest('.blocks-storyline'));
-            let mystyle = document.createElement('style');
-            mystyle.id = 'stylepatch';
-            mystyle.textContent = 'body,#wrapper,.slide .dropin-wrap rect[id^="slide-bg-"] { background-color: ' + style.backgroundColor + ' !important; fill: rgba(0,0,0,0) !important; }';
-            document.body.appendChild(mystyle);
-            mystyle = parent.document.createElement('style'); // shrink the padding on the parent frame
-            mystyle.textContent = '.blocks-storyline__wrapper { padding: 0 1.9999998rem !important; }';
-            parent.document.body.appendChild(mystyle);
+    //     // shrink the padding on the parent frame
+    //     // mystyle.textContent = 'body,#wrapper,.slide .dropin-wrap rect[id^="slide-bg-"] { background-color: ' + style.backgroundColor + ' !important; fill: rgba(0,0,0,0) !important; }';
+    //     // let style = parent.window.getComputedStyle(IFRAME.closest('.blocks-storyline'));
+        if (!parent.document.querySelector('#shrinkpatch')) {
+            let pstyle = document.createElement('style');
+            pstyle.id = 'shrinkpatch';
+            pstyle = parent.document.createElement('style');
+            pstyle.textContent = '.blocks-storyline__wrapper { padding: 0 1.9999998rem !important; }';
+            parent.document.body.appendChild(pstyle);
         }
     } catch(e) {}
 
@@ -108,16 +151,24 @@ if (window.parent !== window.self && !isCrossDomain()) {
     try {
         if (runtimeWindow.hasOwnProperty('courseData')) {
             let c = JSON.parse(atob(runtimeWindow.courseData)).course;
-            // console.log('found course data (published)', c);
+            // console.log('found course data', c);
             courseTitle = c.title;
             contextId = c.id;
+            titleSource = 'courseData';
         } else if (runtimeWindow.hasOwnProperty('courseId')) {
-            // console.log('found course id (runtime?)');
+            console.info('testing for courseId');
+            // console.log('found course id');
             courseTitle = runtimeWindow.courseId;
             contextId = runtimeWindow.courseId;
-        } else {
+            titleSource = 'courseId';
+        } else if (runtimeWindow) {
             // console.log('did not find course title');
-            courseTitle = parent.document.title;
+            courseTitle = runtimeWindow.document.title;
+            contextId = window.name;
+            titleSource = 'runtimeWindow';
+        } else {
+            courseTitle = document.title;
+            titleSource = 'self';
         }
     } catch(e) {}
 
@@ -137,24 +188,24 @@ if (window.parent !== window.self && !isCrossDomain()) {
 }
 
 // if the player can find a scorm runtime, get it so that we can identify the user (otherwise we'll use a sessionStorage id)
-const lmsAPI = findLMSAPI(this);
 let user = temporaryId('user');
 let [myName, myId] = [user, user];
+const lmsAPI = findLMSAPI(this);
 if (lmsAPI) {
     myName = lmsAPI.GetStudentName();
     myId = lmsAPI.GetStudentID();
 }
+const digest = createDigest(myName + myId);
 
 // load the data from the server and set the storyline variable
 function LOAD_DATA() {
-    Promise.resolve( createDigest(myName + myId) )
-    .then((digest) => {
-        let url = new URL(`${SERVER}/${digest}/${contextId}/${BLOCK}/`);
+    SERVER.then((endpoint) => {
+        let url = new URL(`${endpoint}/${digest}/${contextId}/${BLOCK}/`);
         return fetch(url.toString(), {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': md5(location.origin),
+                'Authorization': MD5(location.origin),
                 'Cache-Control': 'no-cache'
             },
             cache: "no-store"
@@ -175,21 +226,25 @@ function LOAD_DATA() {
 
 // store the storyline variable on the server
 function STORE_DATA() {
-    Promise.resolve( createDigest(myName + myId) )
-    .then((digest) => {
-        let url = new URL(`${SERVER}/${digest}/${contextId}/${BLOCK}/`);
+    SERVER.then((endpoint) => {
+        let url = new URL(`${endpoint}/${digest}/${contextId}/${BLOCK}/`);
         return fetch(url.toString(), {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': md5(location.origin),
+                'Authorization': MD5(location.origin),
                 'Cache-Control': 'no-cache'
             },
             body: JSON.stringify({
+                instance: inst,
+                ts: getLocalTime(),
                 course: courseTitle,
+                source: titleSource,
                 page: pageTitle,
+                kind: 'textentry',
                 question: questionText,
-                answer: STORYLINE.GetVar(VARIABLE)
+                answer: STORYLINE.GetVar(VARIABLE),
+                version: 20250207
             }),
             cache: "no-store"
         });
@@ -207,7 +262,6 @@ function STORE_DATA() {
 }
 
 // main
-// console.log('Action', Action);
 switch (Action) {
     case "store":
         STORE_DATA();
